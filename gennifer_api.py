2840d624c5963ccdd8cf4e53b5c35e2e4e1fffa2
Sonia Bendre <65863659+SoniaBendre@users.noreply.github.com>
2023-04-18 18:32:52 -0400

    Update gennifer_api.py

--- gennifer_api.py
+++ gennifer_api.py
@@ -6,12 +6,15 @@ import ppcor
 DATASET_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sample_data')
 
 def generateInputs(dataset_uri):
+    # read in ppcor.r file and pass it into run command below.
+    # need a file server to handle media files, docker volumes
     inputExpr = pd.read_csv(os.path.join(DATASET_PATH, dataset_uri), header=0, index_col=0)
     geneNames = inputExpr.index.values
     inputExpr.index = geneNames
 
 def run(inputExpre):
     # os.system run the R file directly.
+    # need data to persist
     network = pcor(x = np.transpose(inputExpr.values), method = "spearman")
     return network
 
